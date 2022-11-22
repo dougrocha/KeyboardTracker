c73cc4492a237fd38e3cbc03477824594fa30e13
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2022-11-22 14:59:37 -0500

    Added base Form Component

--- apps/api/src/products/controllers/forms.controller.ts
+++ apps/api/src/products/controllers/forms.controller.ts
@@ -1,18 +1,9 @@
 import { Controller } from '@nestjs/common'
-import {
-  Body,
-  Get,
-  Inject,
-  Param,
-  Post,
-  UseGuards,
-} from '@nestjs/common/decorators'
+import { Body, Get, Inject, Param, Post } from '@nestjs/common/decorators'
 import { User } from '@prisma/client'
 
 import { FORMS_SERVICE } from '../../common/constants'
 import { GetCurrentUser } from '../../common/decorators/current-user.decorator'
-import { Roles } from '../../common/decorators/roles.decorator'
-import { CanEditDesigner } from '../../common/guards/designer-owner.guard'
 import { CreateFormDto } from '../dtos/create-form.dto'
 import { FormsService } from '../services/forms.service'
 
@@ -21,8 +12,6 @@ export class FormsController {
   constructor(@Inject(FORMS_SERVICE) private readonly forms: FormsService) {}
 
   @Get(':id/form')
-  @UseGuards(CanEditDesigner)
-  @Roles('DESIGNER', 'USER')
   async getForm(@Param('id') id: string) {
     return await this.forms.findOneByProductId(id)
   }
--- apps/web/src/components/Forms/Form.tsx
+++ apps/web/src/components/Forms/Form.tsx
@@ -1,14 +1,72 @@
 import React from "react"
-import { FormProvider, useForm } from "react-hook-form"
+import {
+  useForm,
+  SubmitHandler,
+  FieldValues,
+  FormProvider,
+  DeepPartial,
+  Resolver,
+} from "react-hook-form"
 
-interface FormProps {
-  children: React.ReactNode
+interface FormProps<T extends FieldValues>
+  extends Omit<React.HTMLAttributes<HTMLFormElement>, "onSubmit"> {
+  defaultValues?: DeepPartial<T>
+  onSubmit: SubmitHandler<T>
+  resolver?: Resolver<T>
 }
 
-const Form = ({ children }: FormProps) => {
-  const methods = useForm()
+function Form<T extends FieldValues>({
+  defaultValues,
+  onSubmit,
+  children,
+  className,
+  resolver,
+  ...rest
+}: React.PropsWithChildren<FormProps<T>>) {
+  const methods = useForm<T>({ defaultValues, resolver })
 
-  return <FormProvider {...methods}>{children}</FormProvider>
+  return (
+    <FormProvider {...methods} {...rest}>
+      <form onSubmit={methods.handleSubmit(onSubmit)} className={className}>
+        {React.Children.map(children, (child) => {
+          if (React.isValidElement(child)) {
+            return React.cloneElement(child, {
+              key: child.props.name,
+              ...child.props,
+              ...methods.register,
+            })
+          }
+          return child
+        })}
+      </form>
+    </FormProvider>
+  )
 }
+
+// const Form = <T>({
+//   defaultValues,
+//   children,
+//   onSubmit,
+// }: FormProps<T> & { children: ReactElement | ReactElement[] }) => {
+//   const methods = useForm({ defaultValues })
+
+//   return (
+//     <FormProvider {...methods}>
+//       <form onSubmit={methods.handleSubmit(onSubmit)}>
+//         {children.map(children, (child: JSX.Element) => {
+//           return child.props.name
+//             ? createElement(child.type, {
+//                 ...{
+//                   key: child.props.name,
+//                   ...child.props,
+//                   ...methods.register,
+//                 },
+//               })
+//             : child
+//         })}
+//       </form>
+//     </FormProvider>
+//   )
+// }
 
 export default Form
