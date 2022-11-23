7a996a10a671e4f9a908610fd4d77327408106d3
Douglas Rocha <79188161+dougrocha@users.noreply.github.com>
2022-11-23 01:36:27 -0500

    Created BaseInput Component

--- apps/web/src/components/Forms/TextArea.tsx
+++ apps/web/src/components/Forms/TextArea.tsx
@@ -1,65 +1,78 @@
 import classNames from "classnames"
+import { get } from "lodash"
 import { ComponentPropsWithoutRef } from "react"
 import { RegisterOptions, useFormContext } from "react-hook-form"
 
+import BaseInput from "./BaseInput"
+import InputErrorIcon from "./InputErrorIcon"
+
 interface TextAreaProps extends ComponentPropsWithoutRef<"textarea"> {
-  label: string
+  label?: string
   id: string
   placeholder?: string
+  height?: `h-${number}`
+  resize?: "none" | "both" | "horizontal" | "vertical"
   helperText?: string
   readOnly?: boolean
+  hideLabel?: boolean
   validation?: RegisterOptions
 }
 
-const TextArea = ({
-  label,
-  id,
-  placeholder,
-  helperText,
-  readOnly = false,
-  validation,
-  ...rest
-}: TextAreaProps) => {
+const TextArea = (props: TextAreaProps) => {
+  const {
+    label,
+    id,
+    placeholder,
+    helperText,
+    readOnly = false,
+    validation,
+    hideLabel = true,
+    height = "h-24",
+    resize,
+    ...rest
+  } = props
+
   const {
     register,
     formState: { errors },
   } = useFormContext()
 
   return (
-    <div>
-      <label htmlFor={id} className="block text-sm">
-        {label}
-      </label>
-      <div className="relative mt-1 w-full">
-        <textarea
-          id={id}
-          readOnly={readOnly}
-          placeholder={placeholder}
-          className={classNames(
-            "block w-full rounded border-none font-medium shadow-sm",
-            readOnly &&
-              "cursor-not-allowed border-gray-300 bg-gray-100 focus:border-gray-300 focus:ring-0",
-            !readOnly &&
-              errors[id] &&
-              "border-red-300 bg-red-50 focus:border-red-300 focus:ring-0",
-            !readOnly &&
-              !errors[id] &&
-              "focus:ring-primary-500 focus:border-primary-500 border-gray-300"
-          )}
-          aria-describedby={id}
-          {...register(id, validation)}
-          {...rest}
-        />
-      </div>
-      <div className="mt-1">
-        {helperText && <p className="text-xs text-gray-500">{helperText}</p>}
-        {errors[id] ? (
-          <span className="text-sm text-red-500">
-            {errors[id]?.message as string}
-          </span>
-        ) : null}
-      </div>
-    </div>
+    <BaseInput
+      id={id}
+      label={label}
+      helperText={helperText}
+      hideLabel={hideLabel}
+    >
+      <textarea
+        id={id}
+        readOnly={readOnly}
+        placeholder={placeholder}
+        className={classNames(
+          "block w-full rounded border-none font-medium shadow-sm",
+          height,
+          resize === "none" && "resize-none",
+          resize === "both" && "resize",
+          resize === "horizontal" && "resize-x",
+          resize === "vertical" && "resize-y",
+          readOnly &&
+            "cursor-not-allowed border-gray-300 bg-gray-100 focus:border-gray-300 focus:ring-0",
+          !readOnly &&
+            get(errors, id) &&
+            "border-red-300 bg-red-50 focus:border-red-300 focus:ring-0",
+          !readOnly &&
+            !get(errors, id) &&
+            "focus:ring-primary-500 focus:border-primary-500 border-gray-300"
+        )}
+        aria-describedby={id}
+        {...register(id, {
+          required: rest.required,
+          ...validation,
+        })}
+        {...rest}
+      />
+      <InputErrorIcon id={id} position="top-right" />
+    </BaseInput>
   )
 }
 
